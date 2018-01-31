65832ea9c0a51f9521ea63316485fb67b01aa076
Administrator <Administrator@smart20123>
2018-01-31 16:19:58 +0900

    한보아 - Country 추가

---
+++ test26/src/main/java/ksmart/project/test26/CountryController.java
@@ -0,0 +1,22 @@
+package ksmart.project.test26;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ksmart.project.test26.service.Country;
+import ksmart.project.test26.service.CountryDao;
+@Controller
+public class CountryController {
+	@Autowired
+	private CountryDao countryDao;
+	@RequestMapping(value="/country/countryList")
+	public String CountryList(Model model) {
+		List<Country> list = countryDao.selectCountryList();
+		model.addAttribute("list",list);
+		return "country/countryList";
+	}
+}
