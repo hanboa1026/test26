8b0f3c3d6fde127c695736b6eb88c8bcabe1f925
Administrator <Administrator@smart20117>
2018-02-06 15:35:21 +0900

    박미란 - memberDao 정렬

--- test26/src/main/java/ksmart/project/test26/service/MemberDao.java
+++ test26/src/main/java/ksmart/project/test26/service/MemberDao.java
@@ -6,14 +6,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberDao {
-		@Autowired
-		private SqlSessionTemplate sqlSessionTemplate;
-		String localName = "ksmart.project.test26.service.memberMapper.";
-		
-		
-		//로그인
-		public Member login(Member member) {
-			return sqlSessionTemplate.selectOne(localName + "selectMemberone", member);
-			
-		}
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	String localName = "ksmart.project.test26.service.memberMapper.";		
+	// 로그인
+	public Member login(Member member) {
+		return sqlSessionTemplate.selectOne(localName + "selectMemberone", member);	
+	}
 }
