7dbafad811390bf2c501eaf27a7ffbb3fb04f379
박미란 <@DESKTOP-IJDRDEU>
2018-02-07 15:17:25 +0900

    김규원 - CompanyService 생성, CompanyController 메소드 호출 방식 service로 변경

--- test26/src/main/java/ksmart/project/test26/CompanyController.java
+++ test26/src/main/java/ksmart/project/test26/CompanyController.java
@@ -12,23 +12,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import ksmart.project.test26.service.Company;
-import ksmart.project.test26.service.CompanyDao;
+import ksmart.project.test26.service.CompanyService;
 
 @Controller
 public class CompanyController {
-	@Autowired
-	private CompanyDao companyDao;
 	
-	// 목록조회
-	@RequestMapping(value="/company/companyList", method = RequestMethod.GET)
-	public String movie(Model model, HttpSession session) {
-		if(session.getAttribute("loginMember") == null ) {
-			return "redirect:/log/login"; 
+	@Autowired
+	private CompanyService companyService;
+	
+	// 목록조회 서비스 이용
+		@RequestMapping(value="/company/companyList", method = RequestMethod.GET)
+		public String movie(Model model, HttpSession session) {
+			if(session.getAttribute("loginMember") == null ) {
+				return "redirect:/log/login"; 
+			}
+			List<Company> list = companyService.selectCompanyList();
+			model.addAttribute("list",list);
+			return "company/companyList";	
 		}
-		List<Company> list = companyDao.selectCompanyList();
-		model.addAttribute("list",list);
-		return "company/companyList";	
-	}
+	
     
 	// 입력페이지 요청
 	@RequestMapping(value="/company/companyInsert", method = RequestMethod.GET)
@@ -39,49 +41,50 @@ public class CompanyController {
        System.out.println("companyAdd 메소드 출력");
        return "company/companyInsert";
 	}
-   
-	// 입력처리 요청
+    
+    // 입력처리 요청 service 이용
     @RequestMapping(value="/companyAdd", method = RequestMethod.POST)
 	public String companyAdd(Company company, HttpSession session) {
     	// 세션이 없으면 로그인 페이지로 리다이렉트 
     	if(session.getAttribute("loginMember") == null ) {
 			return "redirect:/log/login"; 
 		}
-        companyDao.insertCompany(company);
+        companyService.insertCompany(company);
         return "redirect:/company/companyList"; 
 	}
     
-	// 업데이트 처리요청
+    // 업데이트 처리 서비스 이용
     @RequestMapping(value="/company/companyUpdate", method = RequestMethod.POST)
     public String companyUpdate(Company company, HttpSession session){
     	// 세션이 없으면 로그인 페이지로 리다이렉트
     	if(session.getAttribute("loginMember") == null ) {
 			return "redirect:/log/login"; 
 		}
-    	companyDao.updateCompany(company);
+    	companyService.updateCompany(company);
     	return "redirect:/company/companyList";
 	}
     
- 	// 업데이트 정보요청
+    
+    // 업데이트 정보요청 서비스 이용
     @RequestMapping(value="/company/companyUpdate", method = RequestMethod.GET)
-	public String companyUpdate(Model model, HttpSession session, @RequestParam(value="companyId", required=true) int companyId){
-    	// 세션이 없으면 로그인 페이지로 리다이렉트
-    	if(session.getAttribute("loginMember") == null ) {
-			return "redirect:/log/login"; 
-		}
-    	Company company= companyDao.getCompany(companyId);
-    	model.addAttribute("company", company);
-    	return "company/companyUpdate";
-	}
-
-    // 삭제
+   	public String companyUpdate(Model model, HttpSession session, @RequestParam(value="companyId", required=true) int companyId){
+       	// 세션이 없으면 로그인 페이지로 리다이렉트
+       	if(session.getAttribute("loginMember") == null ) {
+   			return "redirect:/log/login"; 
+   		}
+       	Company company= companyService.getCompany(companyId);
+       	model.addAttribute("company", company);
+       	return "company/companyUpdate";
+   	}
+    
+	// 삭제 service 이용
 	@RequestMapping(value="/company/companyDelete", method = RequestMethod.GET)
 	public String companyRemove(HttpSession session, @RequestParam(value="companyId", required=true) int companyId) {
 		// 세션이 없으면 로그인 페이지로 리다이렉트
     	if(session.getAttribute("loginMember") == null ) {
 			return "redirect:/log/login"; 
 		}
-		companyDao.deleteCompany(companyId);
+		companyService.deleteCompany(companyId);
         return "redirect:/company/companyList";
     } 
 }
---
+++ test26/src/main/java/ksmart/project/test26/service/CompanyService.java
@@ -0,0 +1,51 @@
+package ksmart.project.test26.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CompanyService {
+	
+	@Autowired
+	private CompanyDao companyDao;
+	
+	
+	// 목록조희
+	public List<Company> selectCompanyList() {
+		
+		List<Company> list = companyDao.selectCompanyList();
+		return list;
+		
+	}
+	
+	// 입력처리서비스
+	public int insertCompany(Company company) {
+		int row = companyDao.insertCompany(company);
+		return row;
+	}
+	
+	// 회원하나 정보 받기
+	public Company getCompany(int companyId) {
+		Company one = companyDao.getCompany(companyId);
+		return one;
+	}
+	
+	// 업데이트
+	public int updateCompany (Company company) {
+		int row = companyDao.updateCompany(company);
+		return row;
+	}
+	
+	// 삭제
+	public int deleteCompany(int companyId) {
+		int row = companyDao.deleteCompany(companyId);
+		return row;
+	}
+	
+	
+
+}
