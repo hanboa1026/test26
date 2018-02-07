65673a94745feee75b1918270d223ae02bb9a29a
Administrator <Administrator@smart20123>
2018-02-07 10:57:13 +0900

    한보아 - member Controller, Dao, Mapper 업데이트
    (회원가입, 탈퇴, 회원정보조회 완료 / 회원정보수정미완료)

--- test26/src/main/java/ksmart/project/test26/MemberController.java
+++ test26/src/main/java/ksmart/project/test26/MemberController.java
@@ -1,5 +1,7 @@
 package ksmart.project.test26;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.LoggerFactory;
@@ -8,8 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 import ksmart.project.test26.service.Member;
@@ -22,18 +23,71 @@ public class MemberController {
 
 	@Autowired
 	private MemberDao memberDao;
-
+	
+	// 목록조회
+	@RequestMapping(value="", method=RequestMethod.GET)
+	public String memberList(Model model, HttpSession session) {
+		if(session.getAttribute("loginMember") == null ) {
+			return "redirect:/log/login"; 
+		}
+		List<Member> list = memberDao.selectMemberList();
+		model.addAttribute("list",list);
+		return "member/memberList";
+	}
+	
+	// 하나정보 요청
+	@RequestMapping(value="/member/memberInfo", method=RequestMethod.GET)
+	public String memberSelectByNo(HttpSession session, Model model) {
+		Member member = (Member)session.getAttribute("loginMember");
+		Member logMember = memberDao.selectMemberByNo(member.getMemberNo());
+				//memberDao.selectMemberByNo(memberNo);
+		/*Member member = (Member)httpSession.getAttribute("loginMember");
+		+		Member reMember = memberDao.selectMemberInfo(member.getMemberNo());*/
+		model.addAttribute("member", logMember);
+		return "member/memberInfo";
+	}
+	
 	// 입력페이지 요청
 	@RequestMapping(value="/member/memberAdd", method=RequestMethod.GET)
-	public String memberAdd() {
+	public String memberInsert() {
 		return "member/memberInsert";
 	}
 	
-//	// 입력처리 요청
-//	public String memberAdd(Member member) {
-//
-//	}
-//	
+	// 입력처리 요청
+	@RequestMapping(value="/member/memberAdd", method=RequestMethod.POST)
+	public String memberInsert(Member member, HttpSession session) {
+		memberDao.insertMember(member);
+		return "home";
+	}
+	
+	// 업데이트 정보요청
+	@RequestMapping(value="/member/memberModify", method=RequestMethod.GET)
+	public String memberUpdate(HttpSession session, Model model,@RequestParam(value="memberNo", required=true)int memberNo) {	
+		if(session.getAttribute("loginMember") == null ) {
+			return "redirect:/log/login"; 
+		}
+		Member member = (Member)session.getAttribute("loginMember");
+		Member logMember = memberDao.selectMemberByNo(member.getMemberNo());
+		model.addAttribute("member", logMember);
+		memberDao.selectMemberByNo(memberNo);
+		return "member/memberUpdate";		
+	}
+	
+	// 업데이트 처리요청
+	@RequestMapping(value="/member/memberModify", method=RequestMethod.POST)
+	public String memberUpdate(HttpSession session, Model model,@RequestParam(value="memberNo", required=true)Member member) {
+		model.addAttribute("member",member);
+		memberDao.updateMember(member);
+		return "member/memberInfo";		
+	}
+	
+	// 삭제
+	@RequestMapping(value="/member/memberRemove", method=RequestMethod.GET)
+	public String memberDelete(Model model,@RequestParam(value="memberNo", required=true)int memberNo) {
+		memberDao.deleteMember(memberNo);
+		return "/log/logOut";		
+	}
+
 	// 로그인 화면 요청
 	@RequestMapping(value="/log/login", method=RequestMethod.GET)
 	public String Login() {	
@@ -65,13 +119,10 @@ public class MemberController {
 		return "redirect:/";
 	}
 	
+	// 로그아웃 요청
 	@RequestMapping(value = "/log/logOut", method = RequestMethod.GET)
 	public String logout(HttpSession session, Member member){
 			session.invalidate();
-		return "home";
-		
-	}
-	
-
-	
+		return "home";		
+	}	
 }
--- test26/src/main/java/ksmart/project/test26/service/MemberDao.java
+++ test26/src/main/java/ksmart/project/test26/service/MemberDao.java
@@ -1,5 +1,7 @@
 package ksmart.project.test26.service;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -9,10 +11,26 @@ public class MemberDao {
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
 	String localName = "ksmart.project.test26.service.memberMapper.";		
-	/*//회원가입
-	public Member insertMember(Member member) {
-		return sqlSessionTemplate.insert(statement)
-	}*/
+	// 목록조회
+	public List<Member> selectMemberList(){
+		return sqlSessionTemplate.selectList(localName + "selectMemberList");
+	}
+	// 등록
+	public int insertMember(Member member) {
+		return sqlSessionTemplate.insert(localName + "insertMember", member);	
+	}	
+	// 업데이트 정보요청
+	public Member selectMemberByNo(int memberNo) {
+		return sqlSessionTemplate.selectOne(localName + "selectMemberByNo", memberNo);
+	}	
+	// 업데이트
+	public int updateMember(Member member) {
+		return sqlSessionTemplate.update(localName + "updateMember", member);
+	}	
+	// 삭제
+	public int deleteMember(int memberNo) {
+		return sqlSessionTemplate.delete(localName + "deleteMember", memberNo);
+	}
 	// 로그인
 	public Member login(Member member) {
 		return sqlSessionTemplate.selectOne(localName + "selectMemberone", member);	
